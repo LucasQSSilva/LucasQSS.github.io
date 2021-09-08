9a0347da87b44dd32d5cc055c607b108865145fb
Lucas Queiroz <31527218+LucasQSS@users.noreply.github.com>
2021-09-08 00:22:43 -0300

    Add files via upload

--- RobotDijkstra/07a_robot.js
+++ RobotDijkstra/07a_robot.js
@@ -385,7 +385,7 @@ let findShortestPath = (graph, startNode, endNode) => {
  
   // track distances from the start node using a hash object
     let distances = {};
-  distances[endNode] = "Infinity";
+  distances[endNode] = 99;
   distances = Object.assign(distances, graph[startNode]);
  // track paths using a hash object
  console.log("distancias = ", distances);
@@ -536,6 +536,121 @@ export function lazyRobot({place, parcels}, route) {
 
 
 
+
+
+// A Javascript program for Dijkstra's single
+// source shortest path algorithm.
+// The program is for adjacency matrix
+// representation of the graph    
+let V = 9;
+ 
+// A utility function to find the
+// vertex with minimum distance
+// value, from the set of vertices
+// not yet included in shortest
+// path tree
+function minDistance(dist,sptSet)
+{
+     
+    // Initialize min value
+    let min = Number.MAX_VALUE;
+    let min_index = -1;
+     
+    for(let v = 0; v < V; v++)
+    {
+        if (sptSet[v] == false && dist[v] <= min)
+        {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+ 
+// A utility function to print
+// the constructed distance array
+function printSolution(dist)
+{
+    document.write("Vertex \t\t Distance from Source<br>");
+    for(let i = 0; i < V; i++)
+    {
+        document.write(i + " \t\t " +
+                 dist[i] + "<br>");
+    }
+}
+ 
+// Function that implements Dijkstra's
+// single source shortest path algorithm
+// for a graph represented using adjacency
+// matrix representation
+function dijkstra(graph, src)
+{
+    let dist = new Array(V);
+    let sptSet = new Array(V);
+     
+    // Initialize all distances as
+    // INFINITE and stpSet[] as false
+    for(let i = 0; i < V; i++)
+    {
+        dist[i] = Number.MAX_VALUE;
+        sptSet[i] = false;
+    }
+     
+    // Distance of source vertex
+    // from itself is always 0
+    dist[src] = 0;
+     
+    // Find shortest path for all vertices
+    for(let count = 0; count < V - 1; count++)
+    {
+         
+        // Pick the minimum distance vertex
+        // from the set of vertices not yet
+        // processed. u is always equal to
+        // src in first iteration.
+        let u = minDistance(dist, sptSet);
+         
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+         
+        // Update dist value of the adjacent
+        // vertices of the picked vertex.
+        for(let v = 0; v < V; v++)
+        {
+             
+            // Update dist[v] only if is not in
+            // sptSet, there is an edge from u
+            // to v, and total weight of path
+            // from src to v through u is smaller
+            // than current value of dist[v]
+            if (!sptSet[v] && graph[u][v] != 0 &&
+                   dist[u] != Number.MAX_VALUE &&
+                   dist[u] + graph[u][v] < dist[v])
+            {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+     
+    // Print the constructed distance array
+    printSolution(dist);
+}
+ 
+// Driver code
+let graph = [ [ 0, 4, 0, 0, 0, 0, 0, 8, 0 ],
+              [ 4, 0, 8, 0, 0, 0, 0, 11, 0 ],
+              [ 0, 8, 0, 7, 0, 4, 0, 0, 2 ],
+              [ 0, 0, 7, 0, 9, 14, 0, 0, 0],
+              [ 0, 0, 0, 9, 0, 10, 0, 0, 0 ],
+              [ 0, 0, 4, 14, 10, 0, 2, 0, 0],
+              [ 0, 0, 0, 0, 0, 2, 0, 1, 6 ],
+              [ 8, 11, 0, 0, 0, 0, 1, 0, 7 ],
+              [ 0, 0, 2, 0, 0, 0, 6, 7, 0 ] ]
+dijkstra(graph, 0);
+ 
+// This code is contributed by rag2127
+
+
 export function dijkstraRobot({place, parcels}, route) {
   if (route.length == 0) {
     console.log("Estou em", place);
@@ -546,13 +661,13 @@ export function dijkstraRobot({place, parcels}, route) {
     let routes = parcels.map(parcel => {
       if (parcel.place != place) {
         console.log("estou indo para", parcel.place);
-        console.log("nao estou no place", findShortestPath(roadGraph, place, parcel.place));
-        return {route: findShortestPath(roadGraph, place, parcel.place),
+        console.log("nao estou no place", dijkstra(roadGraph, place));
+        return {route: dijkstra(roadGraph, place, parcel.place),
                 pickUp: true};
       } else {
         console.log("estou indo para", parcel.address);
-        console.log("estou no place", findShortestPath(roadGraph, place, parcel.address));
-        return {route: findShortestPath(roadGraph, place, parcel.address),
+        console.log("estou no place", dijkstra(roadGraph, place));
+        return {route: dijkstra(roadGraph, place, parcel.address),
                 pickUp: false};
       }
     });
@@ -566,6 +681,13 @@ export function dijkstraRobot({place, parcels}, route) {
   }
   return {direction: route[0], memory: route.slice(1)};
 }
+
+
+
+
+
+
+
 
 
 
