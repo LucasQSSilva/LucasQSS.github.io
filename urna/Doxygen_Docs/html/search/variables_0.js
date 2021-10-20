6f92a3fcfc22723e2f890a9d2984b76cf4dba57a
Lucas Queiroz <31527218+LucasQSS@users.noreply.github.com>
2021-10-20 04:34:47 -0300

    Add files via upload

---
+++ urna/Doxygen_Docs/html/search/variables_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['_24mysqli_0',['$mysqli',['../conexao_8php.html#a580989e8e3521433691a0351287f6315',1,'conexao.php']]],
+  ['_24nome_5fbanco_1',['$nome_banco',['../conexao_8php.html#ae046e5c50d3a448e84f392ef25d92230',1,'conexao.php']]],
+  ['_24past_2',['$past',['../conexao_8php.html#a94d8da66d7aa78207130ff2af2fbc0e1',1,'conexao.php']]],
+  ['_24senha_3',['$senha',['../conexao_8php.html#a3678c8769c9698fd30581c1016c5f475',1,'conexao.php']]],
+  ['_24servidor_4',['$servidor',['../conexao_8php.html#a4df9ffcff3f83295ac7853191d8bb12c',1,'conexao.php']]],
+  ['_24usuario_5',['$usuario',['../conexao_8php.html#a559c8003f481610caf373b85cc607fb6',1,'conexao.php']]]
+];
