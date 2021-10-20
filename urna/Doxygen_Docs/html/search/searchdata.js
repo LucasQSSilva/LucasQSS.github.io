394e4afebec8678c03f7eb94f5db3d08e59f058c
Lucas Queiroz <31527218+LucasQSS@users.noreply.github.com>
2021-10-20 13:47:46 -0300

    Add files via upload

--- urna/Doxygen_Docs/html/search/searchdata.js
+++ urna/Doxygen_Docs/html/search/searchdata.js
@@ -1,21 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "$ci",
-  1: "c",
-  2: "$i"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "files",
-  2: "variables"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Files",
-  2: "Variables"
 };
 
